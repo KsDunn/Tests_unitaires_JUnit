55cd35ed2922d19448107bb3726e44b34ce1a45d
Hugo Jové <hugo.jove2@gmail.com>
2018-11-15 08:52:36 +0100

    fix: permission internet

--- app/src/main/java/fr/enssat/jovepoirier/playervideo/MainActivity.java
+++ app/src/main/java/fr/enssat/jovepoirier/playervideo/MainActivity.java
@@ -7,8 +7,7 @@ import android.os.Bundle;
 import android.widget.VideoView;
 public class MainActivity extends AppCompatActivity {
 
-    //private static final Uri VIDEO_SAMPLE = Uri.parse("https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4");
-    private static final String VIDEO_SAMPLE = "https://www.youtube.com/watch?v=Zy5E4wvZM7I";
+    private static final String VIDEO_SAMPLE = "https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4";
     private VideoView mVideoView;
 
     @Override
@@ -42,14 +41,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initializePlayer() {
-
-        Uri uri = Uri.parse(VIDEO_SAMPLE);
-        mVideoView.setVideoURI(uri);
+        Uri videoUri = getMedia(VIDEO_SAMPLE);
+        mVideoView.setVideoURI(videoUri);
         mVideoView.start();
     }
 
     private void releasePlayer() {
         mVideoView.stopPlayback();
+    }
+
+    private Uri getMedia(String media) {
+        return Uri.parse(media);
     }
 
 }
