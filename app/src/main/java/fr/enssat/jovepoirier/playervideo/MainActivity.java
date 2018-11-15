411c6d0ac3ef86c832b929879878e4045ddd82ab
Hugo Jové <hugo.jove2@gmail.com>
2018-11-15 09:27:12 +0100

    feat: add controller

--- app/src/main/java/fr/enssat/jovepoirier/playervideo/MainActivity.java
+++ app/src/main/java/fr/enssat/jovepoirier/playervideo/MainActivity.java
@@ -1,14 +1,20 @@
 package fr.enssat.jovepoirier.playervideo;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 public class MainActivity extends AppCompatActivity {
 
     private static final String VIDEO_SAMPLE = "https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4";
+    private static final String PLAYBACK_TIME = "play_time";
     private VideoView mVideoView;
+    private int mCurrentPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mVideoView = findViewById(R.id.videoview);
+
+        if (savedInstanceState != null) {
+            mCurrentPosition = savedInstanceState.getInt(PLAYBACK_TIME);
+        }
+
+        MediaController controller = new MediaController(this);
+        controller.setMediaPlayer(mVideoView);
+        mVideoView.setMediaController(controller);
+
+
     }
 
     @Override
@@ -43,7 +59,24 @@ public class MainActivity extends AppCompatActivity {
     private void initializePlayer() {
         Uri videoUri = getMedia(VIDEO_SAMPLE);
         mVideoView.setVideoURI(videoUri);
+
+        if (mCurrentPosition > 0) {
+            mVideoView.seekTo(mCurrentPosition);
+        } else {
+            // Skipping to 1 shows the first frame of the video.
+            mVideoView.seekTo(1);
+        }
+
         mVideoView.start();
+
+        mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                Toast.makeText(MainActivity.this, "Vidéo terminée",
+                        Toast.LENGTH_SHORT).show();
+                mVideoView.seekTo(1);
+            }
+        });
     }
 
     private void releasePlayer() {
@@ -54,4 +87,10 @@ public class MainActivity extends AppCompatActivity {
         return Uri.parse(media);
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt(PLAYBACK_TIME, mVideoView.getCurrentPosition());
+    }
 }
