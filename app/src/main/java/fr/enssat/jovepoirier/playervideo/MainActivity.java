12470c0dc7331aaadf14aa71b0d7ce75d240fb3c
Hugo Jové <hugo.jove2@gmail.com>
2018-11-15 08:37:18 +0100

    feat: add player

--- app/src/main/java/fr/enssat/jovepoirier/playervideo/MainActivity.java
+++ app/src/main/java/fr/enssat/jovepoirier/playervideo/MainActivity.java
@@ -1,13 +1,55 @@
 package fr.enssat.jovepoirier.playervideo;
 
+import android.net.Uri;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.VideoView;
 public class MainActivity extends AppCompatActivity {
+
+    //private static final Uri VIDEO_SAMPLE = Uri.parse("https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4");
+    private static final String VIDEO_SAMPLE = "https://www.youtube.com/watch?v=Zy5E4wvZM7I";
+    private VideoView mVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mVideoView = findViewById(R.id.videoview);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        initializePlayer();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            mVideoView.pause();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        releasePlayer();
+    }
+
+    private void initializePlayer() {
+
+        Uri uri = Uri.parse(VIDEO_SAMPLE);
+        mVideoView.setVideoURI(uri);
+        mVideoView.start();
+    }
+
+    private void releasePlayer() {
+        mVideoView.stopPlayback();
+    }
+
 }
